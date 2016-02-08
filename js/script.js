06b466bf67796815302e36686b39648a7f065be8
Danko-M <dankomilutinovic@gmail.com>
2016-02-08 21:35:37 +0100

    code cleanup

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 $(function(){ 
-	//jQuery code here 
+
+	// homepage, section 'Shop smarter' [+] buttons animation
 	var $expandBtn = $('.showP');
 	var $collapseBtn = $('.hideP');
 
 	$($expandBtn).on('mouseover',  function(event) {
 		event.preventDefault();
-		/* Act on the event */
-		// console.log('abc');
-		// $(this).toggleClass('hide').next('p').toggle(300);
 		$(this).hide().next('p').slideToggle(300);
-		// $(this).fadeOut().next('p').slideToggle(300);
 		$(this).siblings('div').animate({ 
 			marginTop: '20px' 
 		}, 300);
@@ -17,10 +14,6 @@ $(function(){
 
 	$($collapseBtn).on('click',  function(event) {
 		event.preventDefault();
-		/* Act on the event */
-		// console.log('abc');
-		// $(this).parent('p').toggleClass('hide').siblings('a').toggleClass('hide');
-		// $(this).parent('p').toggle(300).siblings('a').toggleClass('hide');
 		$(this).parent('p').slideToggle(300, function(){
 			$(this).siblings('a').show(300);
 			$(this).siblings('div').animate({ 
@@ -28,4 +21,5 @@ $(function(){
 		}, 300);
 		});
 	});
+
 });
