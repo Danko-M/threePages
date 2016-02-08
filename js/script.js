dfdac39a396f9bb20c29dddba521a31747e490d7
Danko-M <dankomilutinovic@gmail.com>
2016-02-08 11:32:44 +0100

    added toggle effect to sec-03

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(function(){ 
 	//jQuery code here 
+	var $expandBtn = $('.expand');
+	var $collapseBtn = $('.section-03 p a');
+	$($expandBtn).on('mouseover',  function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		// console.log('abc');
+		// $(this).toggleClass('hide').next('p').toggle(300);
+		$(this).hide().next('p').toggle(300);
+		// $(this).fadeOut().next('p').slideToggle(300);
+	});
+
+	$($collapseBtn).on('click',  function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		// console.log('abc');
+		// $(this).parent('p').toggleClass('hide').siblings('a').toggleClass('hide');
+		// $(this).parent('p').toggle(300).siblings('a').toggleClass('hide');
+		$(this).parent('p').slideToggle(300, function(){
+			$(this).siblings('a').show()
+			// console.log($(this));
+		});
+	});
 });
